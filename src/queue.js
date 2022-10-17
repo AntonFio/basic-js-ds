a8bed217b0dff3d5893a0a70199db87efe806a81
AntonFio <anton_fiolkovski@mail.ru>
2022-10-17 16:07:45 +0300

    feat: add queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+
+ class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+}
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   enqueue(value) {
+    const listNode = new ListNode(value);
+    if (this.length === 0) {
+      this.head = listNode;
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = listNode;
+    }
+  this.length++
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return current.value;
   }
 }
 
--- src/stack.js
+++ src/stack.js
@@ -32,3 +32,4 @@ const { NotImplementedError } = require('../extensions/index.js');
 module.exports = {
   Stack
 };
+
